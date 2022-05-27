6d41fec41ebbc10f9e33de19c58e54ef0a8e3672
TimaQ7 <qozhamqult1@gmail.com>
2022-05-27 17:55:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/notescities/CitiesFragment.java
@@ -0,0 +1,20 @@
+package com.example.notescities;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CitiesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cities, container, false);
+    }
+}
